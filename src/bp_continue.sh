e0aaaf62163013912da6e9ce97ba7be34a42a2d6
leogtzr <leogutierrezramirez@gmail.com>
2017-05-20 15:18:08 -0600

    fix #1

--- src/bp_continue.sh
+++ src/bp_continue.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Leo Gutiérrez R. | leogutierrezramirez@gmail.com
 
-# set -x
-
 # readonly ERROR_BP_FLOW_FILE_NOT_FOUND=68
+readonly BP_FLOW_ENV_FILENAME="${WORK_DIR}/bp_flow.env"
+readonly BP_ERROR_FILE="${WORK_DIR}/.bp.error"
 readonly ERROR_INVALID_ARGUMENT_NUMBER=69
 readonly ERROR_INIT_SCRIPT_NOT_FOUND=70
 readonly ERROR_FLOW_NOT_SUPPORTED=71
@@ -14,7 +14,7 @@ export ERROR_INVALID_ARGUMENT_NUMBER
 help_seq() {
     cat <<-HELP_SEQ
     
-    $0 N.sh
+    ${0} N.sh
 
     N:  number of script where the execution failed.
 
@@ -24,42 +24,42 @@ HELP_SEQ
 help_doc() {
     cat <<-HELP_DOC
     
-    $0 INITIAL_SCRIPT
+    ${0} INITIAL_SCRIPT
 
 HELP_DOC
 }
 
-. bp_flow.env 2> /dev/null || {
-    echo -e "\n[ERROR] bp_flow.env file not found.\n"
+. "${BP_FLOW_ENV_FILENAME}" 2> /dev/null || {
+    echo -e "\n[ERROR] ${BP_FLOW_ENV_FILENAME} file not found.\n"
     exit ${ERROR_BP_FLOW_FILE_NOT_FOUND}
 }
 
-if [ $# -ne 1 -a "${FLOW_TYPE}" = "SEQ" ]; then
+if [[ ${#} != 1 && "${FLOW_TYPE}" = "SEQ" ]]; then
     help_seq
     exit ${ERROR_INVALID_ARGUMENT_NUMBER}
-elif [ $# -ne 1 -a "${FLOW_TYPE}" = "DOC" ]; then
+elif [[ ${#} != 1 && "${FLOW_TYPE}" = "DOC" ]]; then
     help_doc
     exit ${ERROR_INVALID_ARGUMENT_NUMBER}
 fi
 
 # Load lib:
-if [ -f ./bplib.sh ]; then
+if [[ -f ./bplib.sh ]]; then
     . ./bplib.sh
 else
-    echo "[`date '+%F %T'`] [ERROR] bplib.sh NOT found."
+    echo "[$(date '+%F %T')] [ERROR] bplib.sh NOT found."
     exit 76
 fi
 
 if [ "${FLOW_TYPE}" = "SEQ" ]; then
 
-    if [ ! -f ".bp.error" ]; then
+    if [[ ! -f "${BP_ERROR_FILE}" ]]; then
         echo "Nothing to do ... "
         exit 0
     fi
 
     SCRIPT_TO_START=$1
-    awk -F '=' '/^FAILED_SCRIPT/ {print $2}' .bp.error | grep -Eq "^${SCRIPT_TO_START}$" && {
-        START_POINT=`awk -F '=' '/^FAILED_SCRIPT/ {print $2}' .bp.error | cut -f1 -d'.'`
+    awk -F '=' '/^FAILED_SCRIPT/ {print $2}' "${BP_ERROR_FILE}" | grep -Eq "^${SCRIPT_TO_START}$" && {
+        START_POINT=`awk -F '=' '/^FAILED_SCRIPT/ {print $2}' "${BP_ERROR_FILE}" | cut -f1 -d'.'`
         for script in `seq -f "%g.sh" ${START_POINT} 10`; do
             if [ -f "${WORKING_DIR}/${script}" ]; then
                 log_debug "Running: $script"
@@ -74,7 +74,7 @@ if [ "${FLOW_TYPE}" = "SEQ" ]; then
                 fi
             fi
         done
-        rm -f ".bp.error" 2> /dev/null
+        rm --f "${BP_ERROR_FILE}" 2> /dev/null
         log_debug "Finished ... "
         exit 0
     } || {
@@ -87,9 +87,9 @@ elif [ "${FLOW_TYPE}" = "DOC" ]; then
     SCRIPT_TO_START=$1
     grep -Ev '^#' "${FLOW_FILE}" | grep -vE '^$' | grep -Eq "^${SCRIPT_TO_START}:" && {
         
-        SCRIPT_TO_EXECUTE_TMP=`grep -Ev '^#' "${FLOW_FILE}" | grep -vE '^$' | grep -E "^${SCRIPT_TO_START}" | sed -n 1p | awk -F ":" '{print $1}'`
+        SCRIPT_TO_EXECUTE_TMP=$(grep -Ev '^#' "${FLOW_FILE}" | grep -vE '^$' | grep -E "^${SCRIPT_TO_START}" | sed -n 1p | awk -F ":" '{print $1}')
         
-        if [ -z "${SCRIPT_TO_EXECUTE_TMP}" ]; then
+        if [[ -z "${SCRIPT_TO_EXECUTE_TMP}" ]]; then
             echo "Script to execute not found."
             exit 98
         fi
